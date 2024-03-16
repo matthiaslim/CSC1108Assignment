1583a31356ea36e05079d6ea4e08f61aa6e1993c
Matthias Lim <matthias.limeexuan@gmail.com>
2024-03-16 18:09:23 +0800

    Added Source and Destination City and Country columns

--- merge_dataset.py
+++ merge_dataset.py
@@ -2,24 +2,33 @@ import pandas as pd
 from math import radians, cos, sin, asin, sqrt
 
 
-def merge_lat_lon():
-    europe_airports = pd.read_csv('europe_airports.csv')
-    europe_routes = pd.read_csv('europe_routes.csv')
+def merge_city_country(airports_df, routes_df):
+    city_country_data = airports_df[['IATA', 'City', 'Country']]
+    merged_data = pd.merge(routes_df, city_country_data, how='left', left_on='Source airport', right_on='IATA')
 
+    merged_data.drop(columns=['IATA'], inplace=True)
+
+    merged_data = pd.merge(merged_data, city_country_data, how='left',
+                           left_on='Destination airport', right_on='IATA', suffixes=('_source', '_destination'))
+
+    merged_data.drop(columns=['IATA'], inplace=True)
+    return merged_data
+
+
+def merge_lat_lon(airports_df, routes_df):
     # Merge the merged data with Europe data on Source airport
-    merged_data = pd.merge(europe_routes, europe_airports[['IATA', 'Latitude', 'Longitude']], how='left',
+    merged_data = pd.merge(routes_df, airports_df[['IATA', 'Latitude', 'Longitude']], how='left',
                            left_on='Source airport', right_on='IATA')
 
     # Drop the redundant 'IATA' column
     merged_data.drop(columns=['IATA'], inplace=True)
 
     # Merge the merged data with Europe data on Destination airport
-    merged_data = pd.merge(merged_data, europe_airports[['IATA', 'Latitude', 'Longitude']], how='left',
+    merged_data = pd.merge(merged_data, airports_df[['IATA', 'Latitude', 'Longitude']], how='left',
                            left_on='Destination airport', right_on='IATA', suffixes=('_source', '_destination'))
 
     # Drop the redundant 'IATA' column
     merged_data.drop(columns=['IATA'], inplace=True)
-    print(merged_data)
     merged_data.to_csv("europe_flights_without_distance.csv", index=False)
 
 
@@ -35,10 +44,11 @@ def haversine_formula_distance(lat1, lon1, lat2, lon2):
     return c * r
 
 
-def main():
+def calculate_distance():
     data = pd.read_csv("europe_flights_without_distance.csv")
-    data.columns = ["Airline", "Airline ID", "Source airport", "Destination airport",
-                    "Source Latitude", "Source Longitude",
+    data.columns = ["Airline", "Airline ID", "Source Airport IATA", "Destination Airport IATA",
+                    "Source City", "Source Country", "Destination City", "Destination Country", "Source Latitude",
+                    "Source Longitude",
                     "Destination Latitude", "Destination Longitude"]
     data['Distance'] = data.apply(
         lambda row: haversine_formula_distance(row['Source Latitude'], row['Source Longitude'],
@@ -47,6 +57,16 @@ def main():
     data.to_csv("europe_flight_dataset.csv", index=False)
 
 
+def main():
+    europe_airports = pd.read_csv('europe_airports.csv')
+    europe_routes = pd.read_csv('europe_routes.csv')
+
+    modified_europe_routes = merge_city_country(europe_airports, europe_routes)
+
+    print(modified_europe_routes)
+    merge_lat_lon(europe_airports, modified_europe_routes)
+    calculate_distance()
+
+
 if __name__ == "__main__":
-    merge_lat_lon()
     main()
