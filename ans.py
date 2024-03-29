a97f184fd25d23f2650a332fc8afe18310d69786
Edwin <hewzhongxuan6f@gmail.com>
2024-03-29 21:10:48 +0800

    Stare at this

--- ans.py
+++ ans.py
@@ -1,5 +1,40 @@
-{'optimal': {'path': ['LHR', 'BSL', 'BDS'], 'stops': 1, 'cost': 444, 'duration': '06:18'},
- 'shortest distance': {'path': ['LHR', 'BSL', 'BDS'], 'stops': 1, 'cost': 444, 'duration': '06:18'},
- 'least cost': {'path': ['LHR', 'MXP', 'BDS'], 'stops': 1, 'cost': 424, 'duration': '06:19'},
- 'shortest duration': {'path': ['LHR', 'BSL', 'BDS'], 'stops': 1, 'cost': 444, 'duration': '06:18'},
- 'least layovers': {'path': ['LHR', 'STR', 'BDS'], 'stops': 1, 'cost': 450, 'duration': '06:21'}}
+# Single
+{
+    "path": ["LHR", "MXP", "SUF"],
+    "distance": 1905.7793075464888,
+    "stops": 1,
+    "cost": 432,
+    "duration": 6.382224134433111,
+}
+
+# Multi
+[
+    {
+        "path": ["LHR", "AMS", "ZRH", "SUF"],
+        "distance": 2300.7793075464888,
+        "stops": 2,
+        "cost": 432,
+        "duration": 12.382224134433111,
+    },
+    {
+        "path": ["LHR", "AMS"],
+        "distance": 370.44882883109966,
+        "stops": 0,
+        "cost": 96,
+        "duration": 1.4630610360388745,
+    },
+    {
+        "path": ["AMS", "ZRH"],
+        "distance": 602.8328984904839,
+        "stops": 0,
+        "cost": 146,
+        "duration": 1.7535411231131048,
+    },
+    {
+        "path": ["ZRH", "SUF"],
+        "distance": 1136.6473912497793,
+        "stops": 0,
+        "cost": 276,
+        "duration": 2.420809239062224,
+    },
+]
