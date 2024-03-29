8c5dd0862bedba88395bfd8fba7b7a72d5d20177
Matthias Lim <matthias.limeexuan@gmail.com>
2024-03-29 22:42:50 +0800

    stare at THIS

--- ans.py
+++ ans.py
@@ -1,40 +1,29 @@
-# Single
 {
-    "path": ["LHR", "MXP", "SUF"],
-    "distance": 1905.7793075464888,
-    "stops": 1,
-    "cost": 432,
-    "duration": 6.382224134433111,
+    "path": [   # List of airports in the route
+        "Airport1",
+        "Airport2",
+        ...
+        "DestinationAirport"
+    ],
+    "segments": [   # List of segments, each representing a flight or layover
+        {
+            "from": "Airport1",
+            "to": "Airport2",
+            "cost": 100,   # Cost of the flight segment
+            "duration": 2,   # Duration of the flight segment in hours
+            "layover": 0   # Layover time in hours (0 for flight segments)
+        },
+        {
+            "from": "Airport2",
+            "to": "Airport3",
+            "cost": 150,
+            "duration": 1.5,
+            "layover": 2   # Layover time between flights
+        },
+        ...
+    ],
+    "total_stops": 2,   # Total number of stops (layovers)
+    "total_cost": 250,   # Total cost of the entire route
+    "total_duration": 5,   # Total duration of the entire route in hours
+    "total_layover_time": 4   # Total layover time in hours for the entire route
 }
-
-# Multi
-[
-    {
-        "path": ["LHR", "AMS", "ZRH", "SUF"],
-        "distance": 2300.7793075464888,
-        "stops": 2,
-        "cost": 432,
-        "duration": 12.382224134433111,
-    },
-    {
-        "path": ["LHR", "AMS"],
-        "distance": 370.44882883109966,
-        "stops": 0,
-        "cost": 96,
-        "duration": 1.4630610360388745,
-    },
-    {
-        "path": ["AMS", "ZRH"],
-        "distance": 602.8328984904839,
-        "stops": 0,
-        "cost": 146,
-        "duration": 1.7535411231131048,
-    },
-    {
-        "path": ["ZRH", "SUF"],
-        "distance": 1136.6473912497793,
-        "stops": 0,
-        "cost": 276,
-        "duration": 2.420809239062224,
-    },
-]
